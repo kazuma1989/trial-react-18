d84532053652461faa05bfb19be955e914fcc0c3
kazuma1989 <kazuma.ebina@quipper.com>
2021-09-15 16:46:42 +0900

    v18のやり方でrender

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="react/experimental" />
+/// <reference types="react-dom/experimental" />
 /// <reference types="vite/client" />
 
 // https://vitejs.dev/guide/env-and-mode.html#intellisense
