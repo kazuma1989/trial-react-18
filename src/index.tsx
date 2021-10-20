be306bdf160ffd26349f91b1427f899947472373
kazuma1989 <kazuma.ebina@quipper.com>
2021-10-20 17:20:02 +0900

    最低限の実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import { StrictMode, Suspense } from "react"
 import { createRoot } from "react-dom"
 import { SWRConfig } from "swr"
-import { AppConcurrent } from "./AppConcurrent"
-import { AppSync } from "./AppSync"
-import "./color.css"
-import { DoublePane } from "./DoublePane"
 import "./global.css"
 
 createRoot(globalThis.document.getElementById("root")!).render(
@@ -18,15 +14,13 @@ createRoot(globalThis.document.getElementById("root")!).render(
         suspense: true,
       }}
     >
-      <DoublePane>
-        <Suspense fallback={<p>全体読み込み中</p>}>
-          <AppSync />
-        </Suspense>
-
-        <Suspense fallback={<p>全体読み込み中</p>}>
-          <AppConcurrent />
-        </Suspense>
-      </DoublePane>
+      <Suspense fallback={<p>全体読み込み中</p>}>
+        <App />
+      </Suspense>
     </SWRConfig>
   </StrictMode>
 )
+
+function App() {
+  return <div>App</div>
+}
