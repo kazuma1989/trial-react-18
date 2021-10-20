272e0a8c46a3a6a8839d572827a4fc114bb86ff3
kazuma1989 <kazuma.ebina@quipper.com>
2021-10-20 17:48:34 +0900

    サンプル実装

--- src/fakeAPI.ts
+++
@@ -1,7 +0,0 @@
-export function fakeAPI(): Promise<string> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(new Date().toString())
-    }, 1_000)
-  })
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import { StrictMode, Suspense } from "react"
+import { css, cx } from "@emotion/css"
+import { StrictMode, Suspense, useDeferredValue, useState } from "react"
 import { createRoot } from "react-dom"
-import { SWRConfig } from "swr"
-import "./global.css"
+import useSWR, { SWRConfig } from "swr"
 
 createRoot(globalThis.document.getElementById("root")!).render(
   <StrictMode>
@@ -12,6 +12,8 @@ createRoot(globalThis.document.getElementById("root")!).render(
         shouldRetryOnError: false,
         dedupingInterval: 86400_000,
         suspense: true,
+        fetcher: (url) =>
+          delay(1_000).then(() => fetch(url).then((r) => r.json())),
       }}
     >
       <Suspense fallback={<p>全体読み込み中</p>}>
@@ -21,6 +23,72 @@ createRoot(globalThis.document.getElementById("root")!).render(
   </StrictMode>
 )
 
+function delay(ms: number) {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+interface Todo {
+  completed: boolean
+  id: number
+  title: string
+  userId: number
+}
+
 function App() {
-  return <div>App</div>
+  const [page, setPage] = useState(1)
+  const deferredPage = useDeferredValue(page)
+
+  const todos: Todo[] = useSWR(
+    `https://jsonplaceholder.typicode.com/todos?_limit=10&_page=${deferredPage}`
+  ).data
+
+  return (
+    <div>
+      <div>
+        {[1, 2, 3, 4, 5].map((p) => (
+          <button
+            key={p}
+            type="button"
+            onClick={() => {
+              setPage(p)
+            }}
+            className={cx(
+              p === page &&
+                css`
+                  background: var(--focus);
+                `
+            )}
+          >
+            {p}
+          </button>
+        ))}
+      </div>
+
+      <table
+        className={css`
+          table-layout: auto;
+        `}
+      >
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>TITLE</th>
+            <th>COMPLETED</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {todos.map(({ id, title, completed }) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{title}</td>
+              <td>{completed ? "YES" : "NO"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
